52abdbd3b8ac38826ece55141c2bcdba4c55eefd
SMDC <148770619+carl-smdc@users.noreply.github.com>
2024-10-22 13:26:28 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-/*!
- * Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
- * Copyright 2013-2023 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
- */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function onReCaptchaSuccess() {
+  // Enable the submit button after successful reCAPTCHA validation
+  document.getElementById("submitBtn").disabled = false;
+}
